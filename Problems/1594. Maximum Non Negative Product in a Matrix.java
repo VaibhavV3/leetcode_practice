dd8f832fa4b4644627268e97cee4feacd78895d1
VaibhavV3 <vaibhavwalekar2013@gmail.com>
2020-09-22 19:07:51 +0530

    Update 1594. Maximum Non Negative Product in a Matrix.java
    code without recursion

--- Problems/1594. Maximum Non Negative Product in a Matrix.java
+++ Problems/1594. Maximum Non Negative Product in a Matrix.java
@@ -1,24 +1,25 @@
 class Solution {
-    int mod = 1000000007;
-    public int maxProductPath(int[][] grid) {
-        return (int) (maxPath(grid,0,0,1L)%mod );
-        
-    }
-    
-    public long maxPath(int[][] grid, int i, int j, long product){
-        if(i == grid.length-1 && j == grid[i].length-1){
-                return (grid[i][j])*(product);
+    public int maxProductPath(int[][] g) {
+        int m = g.length, n = g[0].length, mod = 1_000_000_007;
+        long dp[][][] = new long[m][n][2];
+        dp[0][0] = new long[]{g[0][0], g[0][0]};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) continue;
+                long a = 0, b = 0;
+                if (i == 0) {
+                    dp[i][j][0] = dp[i][j][1] = g[i][j] * dp[i][j - 1][0];
+                } else if (j == 0) { 
+                    dp[i][j][0] = dp[i][j][1] = g[i][j] * dp[i - 1][j][0];
+                } else {
+                    a = g[i][j] * Math.max(dp[i][j - 1][0], dp[i - 1][j][0]);
+                    b = g[i][j] * Math.min(dp[i][j - 1][1], dp[i - 1][j][1]);
+                    dp[i][j][0] = Math.max(a, b);
+                    dp[i][j][1] = Math.min(a, b);
+                }
+            }
         }
-        if(grid[i][j]==0) return 0;
-        product = (grid[i][j])*(product);
-        if(i < grid.length-1 && j < grid.length-1){
-            return Math.max(maxPath(grid, i+1, j,product), maxPath(grid, i , j+1,product));
-        }else if(j < grid.length-1){
-            return maxPath(grid, i , j+1,product);
-        }else if(i < grid.length-1){
-            return maxPath(grid, i+1, j,product);
-        }
-        return 0;
+        if (dp[m - 1][n - 1][0] < 0) return -1;
+        return (int) ((dp[m - 1][n - 1][0]) % mod);
     }
 }
-
